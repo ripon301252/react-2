f5a4964a6c071c3bef2c5a6940f884dc399e0ef4
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2025-01-15 20:31:20 +0600

    Delete friend.jsx

--- friend.jsx
+++
@@ -1,9 +0,0 @@
-export default function Friend({friend}) {
-    const {name, email} = friend;
-    return(
-        <div style={{textAlign : 'center', border : '2px solid red', borderRadius : '15px', marginBottom : '10px', width : '400px', margin: '10px auto'}}>
-            <h4>Name : {name}</h4>
-            <p>Email : {email}</p>
-        </div>
-    )
-}
